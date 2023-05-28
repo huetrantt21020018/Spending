7808d1a55249706251a378f37fd6cd6babce3eeb
huetrantt21020018 <21020018@vnu.edu.vn>
2023-05-28 10:51:33 +0700

    initial commit

---
+++ app/src/main/java/ie/app/SpendActivity.java
@@ -0,0 +1,14 @@
+package ie.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SpendActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spend);
+    }
+}
