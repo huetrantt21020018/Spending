66130c4d6cf738cc51469b496d552b4cb07b988c
huetrantt21020018 <21020018@vnu.edu.vn>
2023-05-28 16:13:20 +0700

    add button

--- app/src/main/java/ie/app/SpendActivity.java
+++ app/src/main/java/ie/app/SpendActivity.java
@@ -3,12 +3,22 @@ package ie.app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
 
 public class SpendActivity extends AppCompatActivity {
+
+    private Button spendButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        spendButton = (Button) findViewById(R.id.spendButton);
+        if (spendButton != null)
+        {
+            Log.v("Spend!", "Really got the spend button");
+        }
+
         setContentView(R.layout.activity_spend);
     }
 }
